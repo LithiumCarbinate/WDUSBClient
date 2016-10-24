ce942a5c2025e74ffa33ec19deed65c10d121589
sixleaves <18649772243@163.com>
2016-10-25 03:01:55 +0800

    add WDUSBClientLib for easy write program

---
+++ WDUSBClient/WDUSBClientLib/WDUSBClientLib.h
@@ -0,0 +1,19 @@
+//
+//  WDUSBClientLib.h
+//  WDUSBClientLib
+//
+//  Created by sixleaves on 2016/10/25.
+//  Copyright © 2016年 netdragon. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WDUSBClientLib.
+FOUNDATION_EXPORT double WDUSBClientLibVersionNumber;
+
+//! Project version string for WDUSBClientLib.
+FOUNDATION_EXPORT const unsigned char WDUSBClientLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WDUSBClientLib/PublicHeader.h>
+
+
---
+++ WDUSBClient/WDUSBClientLib/WDUSBClientLib.m
@@ -0,0 +1,13 @@
+//
+//  WDUSBClientLib.m
+//  WDUSBClientLib
+//
+//  Created by sixleaves on 2016/10/25.
+//  Copyright © 2016年 netdragon. All rights reserved.
+//
+
+#import "WDUSBClientLib.h"
+
+@implementation WDUSBClientLib
+
+@end
